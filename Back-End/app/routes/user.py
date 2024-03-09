9bed49b2060d7e3bdd15ad3a84e28945ead007b1
Muhammad Mahad Mir <mahad@Muhammads-MacBook-Air.local>
2024-03-09 21:18:03 +0000

    crud for user

--- Back-End/app/routes/user.py
+++ Back-End/app/routes/user.py
@@ -26,11 +26,14 @@ def users():
 @user_bp.route('/users/new', methods=['POST'])
 def create_user():
     try:
-        cursor = mongo.db.user_collection.find()
-        data = [convert_document(document) for document in cursor]
-        data = [x for x in data if x["role"] == "customer"]
-        if data:
-            return jsonify(data)
+        data = request.json
+        user_dict = {}
+        for k, v in data.items():
+            user_dict[k] = v
+        res = mongo.db.user_collection.insert_one(user_dict)
+        res = mongo.db.user_collection.find_one(user_dict)
+        res = convert_document(res)
+        return jsonify(res), 200
     except Exception as e:
         return f'Error fetching data: {e}'
     
@@ -42,7 +45,7 @@ def get_user_details(user_id):
         data = [convert_document(document) for document in cursor]
         data = [x for x in data if x["user_id"] == user_id]
         if data:
-            return jsonify(data)
+            return jsonify(data[0]), 200
     except Exception as e:
         return f'Error fetching data: {e}'
 
@@ -63,8 +66,7 @@ def edit_user(user_id):
         res = mongo.db.user_collection.update_one(query, {'$set': data})
         
         if res.modified_count > 0:
-            result = {f'message': 'User {user_id} processed successfully', 'data': user_dict}
-            return jsonify(result)
+            return jsonify(user_dict), 200
         else:
             return jsonify({'error': 'Item not found or no changes made'}), 404
 
@@ -72,8 +74,28 @@ def edit_user(user_id):
         return f'Error: {e}'
     
 
-@user_bp.route('/users/<user_id>/delete', methods=['POST'])
+@user_bp.route('/users/<user_id>/delete', methods=['GET'])
 def delete_user(user_id):
-    pass
+    try:
+        user_dict = {"user_id": user_id}
 
+
+        item_to_delete = mongo.db.user_collection.find_one(user_dict)
+
+
+
+        if item_to_delete != None:
+
+            res = mongo.db.user_collection.delete_one(user_dict)
+            
+            
+            
+            if res.deleted_count > 0:
+                return jsonify(convert_document(item_to_delete)), 200
+        else:
+                return jsonify({'error': 'Item not found or no changes made'}), 404
+
+    except Exception as e:
+        return f'Error: {e}'
+    
 
